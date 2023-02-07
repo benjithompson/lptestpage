6f78aaeb169601c3a35d2b932fb4665c589ad3be
Ben Thompson <mail@benjithompson.com>
2023-02-07 07:59:25 -0800

    adding back fetch

--- web/js/util/newpage.js
+++ web/js/util/newpage.js
@@ -9,3 +9,13 @@ function newPage() {
 }
 
 // waitForTag(newPageInit);
+fetch('https://va.v.liveperson.net/api/account/75554483/monitoring/visitors/FiN2NjYzNmNTk5MDVjNjY2/visits/current/state?v=1&filter=agent&sid=RqVR_zWDR_a7T4sRG8At1A', {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer 809a9474e44afc326d3ab1bf52b9ee84e93581dbf2ff01e72e261e24b57565c1'
+        
+    },
+})
+.then(response => response.json())
+.then(response => console.log(JSON.stringify(response)))
