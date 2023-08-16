26a3eed402d6cee14baf512c910b0cbb7e08dea9
Ben Thompson <mail@benjithompson.com>
2023-08-16 13:58:19 -0700

    iframe

--- js/util/closeConversation.js
+++ js/util/closeConversation.js
@@ -2,6 +2,7 @@ const closeConversation = async () => {
     document.querySelector('.lp_close').click();
     await sleep(500);
     document.querySelector('.lp_confirm_button').click();
+    console.log("close convo");
 }
 
 const sleep = async (milliseconds) => {
